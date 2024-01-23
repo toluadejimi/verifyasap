536df9fb05293b66c3969082e3e9f0eb10f92f17
Jimmy <toluadejimi@gmail.com>
2024-01-23 20:49:12 +0100

    hjklkj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -616,7 +616,7 @@
                             <div class="widget">
                                 <h4 class="widget-title  mb-3">Learn More</h4>
                                 <ul class="list-unstyled text-reset mb-0">
-                                    <li><a href="/">About Us</a></li>
+                                    <li><a href="about-us">About Us</a></li>
                                        <li><a href="policy">Policies</a></li>
 
 
