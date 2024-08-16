911109bd2a85616ab10c795cbe11e9de7307cc1d
Jimmy <toluadejimi@gmail.com>
2024-08-16 08:15:00 +0100

    Out of stock number updated

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,10 +7,10 @@
     <!-- Mobile Specific Metas -->
     <meta name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover">
-    <title>Pay</title>
+    <title>Verify Asap</title>
     <!-- Favicon and Touch Icons  -->
-    <link rel="shortcut icon" href="{{url('')}}/public/assets/assets/images/logo.png"/>
-    <link rel="apple-touch-icon-precomposed" href="{{url('')}}/public/assets/assets/images/logo.png"/>
+    <link rel="shortcut icon" href="{{url('')}}/public/assets/assets/app/icons/icon-96x96.png"/>
+    <link rel="apple-touch-icon-precomposed" href="{{url('')}}/public/assets/assets/app/icons/icon-96x96.png"/>
     <!-- Font -->
     <link rel="stylesheet" href="{{url('')}}/public/assets/assets/fonts/fonts.css"/>
     <!-- Icons -->
