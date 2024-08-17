eee4a6874bae14ac60f89483b97247a62d7b78cf
Jimmy <toluadejimi@gmail.com>
2024-08-17 14:08:36 +0100

    Out of stock number updated

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -62,113 +62,6 @@
                 @endif
 
 
-                <div class="col-xl-12 col-md-12 col-sm-12 p-5 ">
-
-                    <p class="mb-5 text-muted d-flex justify-content-center"> Choose your preferred country and
-                        service
-                    </p>
-
-                    <form action="check-av" method="POST">
-                        @csrf
-
-                        <h6 for="country" class="mb-3 mt-3 text-muted d-flex justify-content-center"> 🌍 Select
-                            Country </h6>
-                        <div class="search-container d-flex justify-content-center mb-3">
-                            <input type="text" id="search" name="country_name"
-                                   class="search-box d-flex justify-content-center"
-                                   placeholder="Search country"
-                                   onkeyup="filterItems()" onclick="toggleDropdown()">
-                            <div id="dropdown" class="dropdown">
-                                @foreach ($countries as $data)
-                                    <div class="item" data-id="{{ $data->ID }}" onclick="selectCountry(this)">
-                                        {{ $data->name }}
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-
-
-                        <h6 for="service" class="mb-3 mt-3 text-muted d-flex justify-content-center"> 📞 Select
-                            Service </h6>
-                        <div class="search-container d-flex justify-content-center mb-3">
-                            <input type="text" id="searchservice" class="search-box d-flex justify-content-center"
-                                   placeholder="Search service"
-                                   onkeyup="filterItemsservice()" onclick="toggleDropdownservice()">
-                            <div id="dropdownservice" class="dropdown">
-                                @foreach ($services as $data)
-                                    <div class="item" data-id="{{ $data->ID }}" onclick="selectService(this)">
-                                        {{ $data->name }}
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-
-                        <input type="hidden" name="selectedID" id="selectedID">
-                        <input type="hidden" name="serviceID" id="serviceID">
-
-
-                        <button style="background: #4079E0; font-size:16px;  color: white" type="submit"
-                                class="btn btn-primary btn-lg mt-3">Check Availability
-                        </button>
-
-
-                    </form>
-
-
-                </div>
-
-                @if ($product != null)
-                    <div class="col-xl-12 col-md-12 col-sm-12 p-5 ">
-
-                        <h6> 📶 Service Information</h6>
-                        <div class="card my-4">
-                            <div class="card-body">
-                                <div class="row">
-
-                                    <div class="col-6">
-                                        <label>Country</label>
-                                        <h6>{{ $country_name }}</h6>
-                                    </div>
-
-                                    <div class="col-6">
-                                        <label>Amount</label>
-                                        <h6>NGN {{ number_format($price, 2) }}</h6>
-                                    </div>
-
-                                </div>
-
-
-                                @if(Auth::user()->wallet < $price)
-                                    <a href="fund-wallet"
-                                       class="btn btn-secondary text-white btn-lg">Fund Wallet</a>
-                                @else
-
-                                    <form action="order_now" method="POST">
-                                        @csrf
-
-                                        <input type="text" name="country" hidden value="{{ $count_id }}">
-                                        <input type="text" name="price" hidden value="{{ $price }}">
-                                        <input type="text" name="service" hidden value="{{ $serv }}">
-
-
-                                        <button style="background: #4079E0; font-size:16px; color: white" type="submit"
-                                                class="btn mt-6">Buy Number
-                                            Now
-                                        </button>
-
-                                    </form>
-
-                                @endif
-
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-                @endif
-
 
                 <div class="col-xl-12 col-md-12 col-sm-12 p-2">
 
@@ -240,6 +133,14 @@
                             </div>
 
                         </div>
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+
+                        <a href="/home" style="background: #4079E0; font-size:16px;  color: white" type="submit"
+                                class="btn btn-primary btn-lg mt-3">Buy Number
+                        </a>
+
                     </div>
 
                 </div>
